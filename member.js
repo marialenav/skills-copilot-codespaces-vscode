31ab1b4d021ddf4323e154e04277e27ff9ad2b55
marialenav <56070588+marialenav@users.noreply.github.com>
2023-11-29 22:31:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/skills/views/member.html',
+        controller: 'skillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+
+        }
+
+
+    };
+
+
+}
